f270ac05a34bb5a21ac225abe83938d1f1f661bd
evanwonisch <evanwonisch@users.noreply.github.com>
2020-01-26 14:35:46 +0000

    Initial commit

---
+++ neuralNetwork.js
@@ -0,0 +1,379 @@
+class Matrix {
+
+    //Matrix multiplication
+    static multiply(matrix, vector){ 
+        if(matrix[0].length==vector.length){
+            var result = [];
+            for(var i=0;i<matrix.length;i++){
+                var sum =0;
+                for(var j=0;j<matrix[0].length;j++){
+                    sum+=matrix[i][j]*vector[j];
+                }
+                result[i] = sum;
+            }
+            return result;
+        }
+        else {
+            console.log("Wrong dimensions!");
+        }
+    }
+
+    //Vector addition
+    static vectorAdd(vector, addend){
+        var result = [];
+        if(vector.length == addend.length) {
+            for(var i=0;i<vector.length;i++){
+                result[i] = parseFloat(vector[i]) + parseFloat(addend[i]);
+            }
+            return result;
+        }
+        else {
+            console.log("Wrong dimensions!");
+        }
+    }
+
+    //Matrix addition
+    static matrixAdd(matrix1, matrix2){
+        var result = [];
+        if(matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length) {
+            for(var i=0;i<matrix1.length;i++) {
+                result[i] = [];
+                for(var j =0;j<matrix1[0].length;j++){
+                    result[i][j] = matrix1[i][j]+matrix2[i][j];
+                }
+            }
+            return result;
+        }
+        else {
+            console.log("Wrong dimensions!");
+        }
+    }
+
+     //Vector substraction
+     static vectorSubstract(vector, subtractor){
+         var result = [];
+        if(vector.length == subtractor.length) {
+            for(var i=0;i<vector.length;i++){
+                result[i] = parseFloat(vector[i]) - parseFloat(subtractor[i]);
+            }
+            return result;
+        }
+        else {
+            console.log("Wrong dimensions!");
+        }
+    }
+
+     //Matrix substraction
+     static matrixSubstract(matrix1, matrix2){
+        var result = [];
+        if(matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length) {
+            for(var i=0;i<matrix1.length;i++) {
+                result[i] = [];
+                for(var j =0;j<matrix1[0].length;j++){
+                    result[i][j] = matrix1[i][j] - matrix2[i][j];
+                }
+            }
+            return result;
+        }
+        else {
+            console.log("Wrong dimensions!");
+        }
+    }
+
+    //Scalar Product
+    static vectorScalarProduct(vector1, vector2) {
+        var result = 0;
+        if(vector1.length == vector2.length){
+            for(var i = 0; i < vector1.length; i++) {
+                    result += vector1[i] * vector2[i];
+            }
+            return result;
+        } else {
+            console.log("Wrong dimensions!");
+        }
+
+    }
+
+    //Norm
+    static vectorNorm(vector) {
+        var scalar_product = Matrix.vectorScalarProduct(vector, vector);
+        var result = Math.sqrt(scalar_product);
+        return result;
+    }
+
+    //Transposing Matrix
+    static transposeMatrix(matrix)
+    {
+        var result = Matrix.create(matrix[0].length, matrix.length);
+        for(var i=0;i<matrix.length;i++) {
+            for(var j=0;j<matrix[0].length;j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    //Creating Matrix or Vector filled with 0
+    static create(rows, cols){
+        var data = [];
+        for(var i = 0; i < rows; i++)
+        {
+            if(cols==0) {
+                data[i] = 0;
+            }
+            else {
+                data[i] = [];
+                for(var j = 0; j < cols; j++) {
+                    data[i][j] = 0;
+                }
+            }
+        }
+        return data;
+    }
+
+    //Creating random Matrix or Vector
+    static createRandom(rows, cols, maxValue){
+        var data = [];
+        for(var i = 0; i < rows; i++)
+        {
+            if(cols==0) {
+                data[i] = Math.random()*maxValue;
+            }
+            else {
+                data[i] = [];
+                for(var j = 0; j < cols; j++) {
+                    data[i][j] = Math.random() * 2 * maxValue - maxValue;
+                }
+            }
+        }
+        return data;
+    }
+
+    //Randomising a given Vector 
+    static randomiseVector(vector, maxValue) {
+        for(var i=0;i<vector.length;i++) {
+            vector[i] = Math.random() * maxValue * 2 - maxValue;
+        }
+    }
+
+    //Randomising a given Matrix
+    static randomiseMatrix(matrix, maxValue) {
+        for(var i = 0;i < matrix.length; i++) {
+            for(var j = 0;j<matrix[0].length;j++) {
+                matrix[i][j] =  Math.random() * maxValue * 2 - maxValue;
+            }
+        }
+    }
+}
+
+//dimensioms = [2,3,4,5,1];
+class NeuralNetwork {
+    constructor(dimensions) {
+
+        this.dimensions = dimensions;
+        this.layers = [];
+
+        for(var i = 0; i < dimensions.length; i++) {
+            this.layers[i] = Matrix.create(dimensions[i], 1);
+            
+        }
+
+        this.weightsMatrixes = [];
+
+        for(var i = 0; i < dimensions.length - 1; i++){
+            this.weightsMatrixes[i] = Matrix.create(dimensions[i + 1], dimensions[i]);
+        }
+
+        this.biasVectors = [];
+
+        for(var i = 0; i < dimensions.length - 1; i++) {
+            this.biasVectors[i] = Matrix.create(dimensions[i + 1], 1);
+        }
+
+        this.setRandom();
+    }
+
+    //Feedforward Algorithmn
+    calculate(input) {
+
+        if(input.length == this.layers[0].length){
+            this.layers[0] = input;
+        } else {
+            console.log("Wrong dimensions!");
+            return;
+        }
+
+
+        //Feedforward
+
+        for(var i = 1; i < this.layers.length; i++){
+            this.layers[i] = this.activation(Matrix.vectorAdd(Matrix.multiply(this.weightsMatrixes[i - 1],this.layers[i-1]), this.biasVectors[i-1]));
+        }
+
+        return this.layers[this.layers.length - 1];
+    }
+
+    //Randomises Weights and Balances
+    setRandom(){
+        
+        for(var i = 0; i < this.weightsMatrixes.length; i++) {
+            Matrix.randomiseMatrix(this.weightsMatrixes[i], 3);
+        }
+
+        for(var i = 0; i < this.biasVectors.length; i++ ){
+            Matrix.randomiseVector(this.biasVectors[i], 3);
+        }
+    }
+
+    //Implemented Activation Function
+    activation(vector) {
+        var result = [];
+        for (var i = 0; i < vector.length; i++){
+            result[i] = 1/(1 + Math.exp( -vector[i]));
+        }
+        return result;
+    }
+
+    //Generates Random Trainingdata Object Array
+    // trainingdata = [{data: [], target: []}, ...];
+    generateRandomTrainingData(amount) {
+
+        var trainingdata = [];
+        for(var j = 0; j < amount; j++){
+    
+            var input = [];
+            var output = [];
+    
+            for(var i=0;i<this.inputLayer.length;i++){
+                input[i]= Math.random();
+            }
+    
+            for(var i=0;i<this.outputLayer.length;i++){
+                output[i]= Math.random();
+            }
+            
+            trainingdata[j] = {
+                data : input,
+                target : output
+            };
+        }
+        return trainingdata;
+    }
+
+    train(trainingData, max_error, stepSize, minimumRate, max_stuck) {
+
+        var error_now = this.getGeneralError(trainingData);
+        var error_before = error_now;
+        var trainingTensor = this.generateRandomTrainingTensor(stepSize);
+
+        var iterations = 0;      //Number of Total Training Iterations
+        var stuck = 0;           //Gives number of iterations without progress
+        var nOfStucks = 0;       //Gives the rotal number of other achieved local Minima
+
+        console.log("Starting at: Error = " + error_now);
+
+        while(error_now >= max_error) { 
+        //while(iterations < 1000){
+            this.applyTrainingTensor(trainingTensor);
+            error_now = this.getGeneralError(trainingdata);
+
+
+            if(error_now - error_before < -minimumRate * (error_now - max_error) * stepSize * 10){ //Progressing as long as rate is above minimum
+                stuck = 0;
+            } else {
+                this.undoTrainingTensor(trainingTensor); //monoton Fallend (minimumRate)
+                trainingTensor = this.generateRandomTrainingTensor(stepSize);
+                error_now = this.getGeneralError(trainingdata);
+                stuck++;
+            }
+
+            error_before = error_now;
+            iterations++;
+
+            Plot.addPoint("Error", [iterations, error_now]);
+            Plot.addPoint("Stuck", [iterations, stuck]);
+
+            if(stuck >= max_stuck) { //jumping somewhere else in case of local minima above max_error
+                
+                this.setRandom();
+                error_now = this.getGeneralError(trainingdata);
+                error_before = error_now;
+
+                nOfStucks++;
+                stuck = 0;
+            }
+        }
+
+        console.log("Complete!\n"+"Reached: Error = "+ error_now +"\nIterations: "+iterations+"\nDiscovered local Minima: "+ nOfStucks);
+        return {
+            "iterations": iterations,
+            "final_error": error_now,
+            "nOfStucks": nOfStucks
+        };
+    }
+
+     //Returns the General Error over a Set of TrainingData
+     //generalError = ||targetVector - resultVector||
+    getGeneralError(trainingData){
+
+        var general_error = 0;
+        for(var i = 0; i < trainingData.length; i++) {
+
+            //Result on given Training Object
+            var result = this.calculate(trainingData[i].data);
+
+            //Error
+            var error_vector = Matrix.vectorSubstract(trainingData[i].target, result);
+            var error = Matrix.vectorNorm(error_vector);
+            general_error += error;
+
+        }
+        return general_error/trainingdata.length;
+    }
+    
+    //returns TrainingTensor
+    // obj = {deltaWeightsMatrixes = [][][], deltaBiasVectors = [][]};
+    generateRandomTrainingTensor(speed) {
+        var obj = {};
+
+        obj.deltaWeightsMatrixes = [];
+
+        for(var i = 0; i < this.dimensions.length - 1; i++){
+            obj.deltaWeightsMatrixes[i] = Matrix.createRandom(this.dimensions[i + 1], this.dimensions[i], speed);
+        }
+
+        obj.deltaBiasVectors = [];
+
+        for(var i = 0; i < this.dimensions.length - 1; i++) {
+            obj.deltaBiasVectors[i] = Matrix.create(this.dimensions[i + 1], 1, speed);
+        }
+        return obj;
+    }
+
+    //Applying a given Tensor to the networks weights and biases
+    applyTrainingTensor(trainingTensor){
+
+        for(var i = 0; i < this.weightsMatrixes.length; i++){
+            this.weightsMatrixes[i] = Matrix.matrixAdd(this.weightsMatrixes[i], trainingTensor.deltaWeightsMatrixes[i]);
+        }
+
+        for(var i = 0; i < this.biasVectors.length; i++){
+            this.biasVectors[i] = Matrix.vectorAdd(this.biasVectors[i], trainingTensor.deltaBiasVectors[i]);
+        }
+
+    }
+
+    //Undoes a given trainingTensor
+    undoTrainingTensor(trainingTensor){
+
+        for(var i = 0; i < this.weightsMatrixes.length; i++){
+            this.weightsMatrixes[i] = Matrix.matrixSubstract(this.weightsMatrixes[i], trainingTensor.deltaWeightsMatrixes[i]);
+        }
+
+        for(var i = 0; i < this.biasVectors.length; i++){
+            this.biasVectors[i] = Matrix.vectorSubstract(this.biasVectors[i], trainingTensor.deltaBiasVectors[i]);
+        }
+
+    }  
+}
---
+++ script.js
@@ -0,0 +1,73 @@
+Plot.addPlot("Error",-1000,0,2000,500,"iterations","error");
+Plot.addPlot("Stuck",-1000,550,2000,500,"iterations","stuck");
+Plot.addPlot("Result",1200,0,600,600,"x","y");
+
+var max_error = 0.07;
+var stepSize = 0.3;
+var max_stuck = 30;
+var minimumRate = 0.001;
+
+network = new NeuralNetwork([2,5,5,5,1]);
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "data.json", false);
+xhttp.send();
+var data = JSON.parse(xhttp.responseText);
+
+// network.weightsMatrixes = data.weightsMatrixes;
+// network.biasVectors = data.biasVectors;
+
+var trainingdata = [];
+
+for(var i = 0; i < 4000; i++){
+    var x = Math.random() * 10 - 5;
+    var y = Math.random() * 10 - 5;
+
+    trainingdata[i] = {};
+    trainingdata[i].data = [x, y];
+
+    if( 2.5*(x*x*x) + x*x*x*x >= y){
+        trainingdata[i].target = [0];
+        Plot.addPoint("Result", [x,y]);
+    } else {
+        trainingdata[i].target = [1];
+    }
+}
+
+
+var result = network.train(trainingdata, max_error, stepSize, minimumRate, max_stuck);
+
+// var result = {};
+// result.iterations = 500;
+
+Plot.addLine("Stuck", "x", max_stuck);
+Plot.addLine("Error", "x", max_error);
+
+Plot.drawPlot("Error", 1000 / result.iterations, 200);
+Plot.drawPlot("Stuck", 1000 / result.iterations, 1);
+
+
+for(var i = -100; i < 100; i++){
+    var j = - 100;
+    while(network.calculate([i/20,j/20]) <= 0.5 && j <= 100){
+        j ++;
+        Plot.addPoint("Result", [i/20,j/20]);
+    }
+
+}
+
+Plot.drawPlot("Result", 50,50);
+
+
+var obj = {"weightsMatrixes" : network.weightsMatrixes,
+            "biasVectors": network.biasVectors
+};
+
+var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+
+var a = document.createElement('a');
+a.href = 'data:' + data;
+a.download = 'data.json';
+a.innerHTML = 'download JSON';
+a.click();
+
