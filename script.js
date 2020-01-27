e5bb155f8b395d9bd64d997552bf7973101acbbd
evanwonisch <evanwonisch@users.noreply.github.com>
2020-01-27 16:57:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,6 @@ network = new NeuralNetwork([2,10,10,1]);
 var iterations = 0;
 var training_tensor = network.generateRandomTrainingTensor(0.1);
 var trainingdata = [];
-for(var i = 0; i < 4000; i++){
-    var x = Math.random() * 10 - 5;
-    var y = Math.random() * 10 - 5;
-
-    trainingdata[i] = {};
-    trainingdata[i].data = [x, y];
-
-    if( 2.5*(x*x*x) + x*x*x*x >= y){
-        trainingdata[i].target = [0];
-    } else {
-        trainingdata[i].target = [1];
-    }
-}
-
 // for(var i = 0; i < 4000; i++){
 //     var x = Math.random() * 10 - 5;
 //     var y = Math.random() * 10 - 5;
@@ -28,12 +14,26 @@ for(var i = 0; i < 4000; i++){
 //     trainingdata[i] = {};
 //     trainingdata[i].data = [x, y];
 
-//     if( x*x*x >= y){
+//     if( 2.5*(x*x*x) + x*x*x*x >= y){
 //         trainingdata[i].target = [0];
 //     } else {
 //         trainingdata[i].target = [1];
 //     }
 // }
+
+for(var i = 0; i < 4000; i++){
+    var x = Math.random() * 10 - 5;
+    var y = Math.random() * 10 - 5;
+
+    trainingdata[i] = {};
+    trainingdata[i].data = [x, y];
+
+    if( x*x*x*0.5 >= y){
+        trainingdata[i].target = [0];
+    } else {
+        trainingdata[i].target = [1];
+    }
+}
 
 setInterval(update, 10);
 var keys = {};
