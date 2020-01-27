68eb94438f02d5c050626f3671bb8c2abcf35fd4
evanwonisch <evanwonisch@users.noreply.github.com>
2020-01-27 15:49:53 +0000

    Update

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-Plot.addPlot("Error",-1000,0,2000,500,"iterations","error");
-Plot.addPlot("Distance to Stuck",-1000,550,2000,500,"iterations","d(stuck)");
-Plot.addPlot("Result",1200,0,600,600,"x","y");
+var canvas = document.getElementById("plot");
+var ctx = canvas.getContext("2d");
+var height = canvas.height = window.innerHeight;
+var width = canvas.width = window.innerWidth;
 
-var max_error = 0.1;
-var stepSize = 0.3;
-var max_stuck = 50;
-var minimumRate = 0.00001;
-var max_iterations = 20000;
-var min_distance_to_Minima = 10;
-
-network = new NeuralNetwork([2,3,3,1]);
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.open("GET", "data.json", false);
-// xhttp.send();
-// var data = JSON.parse(xhttp.responseText);
-
-// network.weightsMatrixes = data.weightsMatrixes;
-// network.biasVectors = data.biasVectors;
-
-//Test
-
+network = new NeuralNetwork([2,10,10,1]);
+var iterations = 0;
+var training_tensor = network.generateRandomTrainingTensor(0.1);
 var trainingdata = [];
-
 for(var i = 0; i < 4000; i++){
     var x = Math.random() * 10 - 5;
     var y = Math.random() * 10 - 5;
@@ -32,44 +16,95 @@ for(var i = 0; i < 4000; i++){
 
     if( 2.5*(x*x*x) + x*x*x*x >= y){
         trainingdata[i].target = [0];
-        Plot.addPoint("Result", [x,y]);
     } else {
         trainingdata[i].target = [1];
     }
 }
 
+// for(var i = 0; i < 4000; i++){
+//     var x = Math.random() * 10 - 5;
+//     var y = Math.random() * 10 - 5;
 
-var result = network.train(trainingdata, max_error, stepSize, minimumRate, max_stuck, max_iterations, min_distance_to_Minima);
+//     trainingdata[i] = {};
+//     trainingdata[i].data = [x, y];
 
+//     if( x*x*x >= y){
+//         trainingdata[i].target = [0];
+//     } else {
+//         trainingdata[i].target = [1];
+//     }
+// }
 
-Plot.addLine("Error", "x", max_error);
-Plot.addLine("Distance to Stuck","x",min_distance_to_Minima);
+setInterval(update, 10);
+var keys = {};
+var error_now = network.getGeneralError(trainingdata);
+var error_before = error_now;
 
-Plot.drawPlot("Error", 1000 / result.iterations, 200);
-Plot.drawPlot("Distance to Stuck", 1000 / result.iterations, 1/2);
-
-
-for(var i = -100; i < 100; i++){
-    var j = - 100;
-    while(network.calculate([i/20,j/20]) <= 0.5 && j <= 100){
-        j ++;
-        Plot.addPoint("Result", [i/20,j/20]);
+function update()
+{
+    if(keys.KeyR) {
+        network.setRandom();
     }
 
+   advance();
+
+    display();
 }
 
-Plot.drawPlot("Result", 50,50);
+function advance(){
+    iterations++;
+
+    network.applyTrainingTensor(training_tensor);
+    error_now = network.getGeneralError(trainingdata);
+
+    if(error_now <= error_before){
+
+    } else {
+        network.undoTrainingTensor(training_tensor);
+        training_tensor = network.generateRandomTrainingTensor(0.1);
+        error_now = network.getGeneralError(trainingdata);
+        error_before = error_now;
+    }
+
+    error_before = error_now;
+}
+
+function display() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,width,height);
+
+    for(var i = -50; i < 50; i++){
+        for(var j = -50; j < 50;j++){
 
 
-// var obj = {"weightsMatrixes" : network.weightsMatrixes,
-//             "biasVectors": network.biasVectors
-// };
+            ctx.fillStyle = toColour(network.calculate([i/10,j/10]));
+            //ctx.fillStyle="red";
+            ctx.fillRect(i*5+350,-j*5+350,5,5);
+        }
+    }
 
-// var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
+    ctx.fillStyle = "rgba(255,0,0.7)";
 
-// var a = document.createElement('a');
-// a.href = 'data:' + data;
-// a.download = 'data.json';
-// a.innerHTML = 'download JSON';
-// a.click();
+    for(var i = 0; i < trainingdata.length; i++){
+        if(trainingdata[i].target == 0){
+            ctx.fillRect(350+trainingdata[i].data[0]*50,350-trainingdata[i].data[1]*50,2,2);
+        }
+    }
 
+    ctx.fillStyle = "white";
+    ctx.font = "20px Georgia";
+    ctx.fillText("Error: " + error_now, 650,500);
+    ctx.fillText("Iterations: " + iterations, 650,550);
+}
+
+function toColour(value){
+    return "rgb(100,"+value*255+","+value*255+")";
+}
+
+document.addEventListener("keydown", e => {
+    keys[e.code] = true;
+});
+
+document.addEventListener("keyup", e => {
+    keys[e.code] = false;
+});
