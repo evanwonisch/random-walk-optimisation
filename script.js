55719b5c9e49ad034cd6bd311d3c1b271ad42c97
evanwonisch <evanwonisch@users.noreply.github.com>
2020-01-27 20:52:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var ctx = canvas.getContext("2d");
 var height = canvas.height = window.innerHeight;
 var width = canvas.width = window.innerWidth;
 
-network = new NeuralNetwork([2,10,10,1]);
+network = new NeuralNetwork([6,4,3,1]);
 var iterations = 0;
-var training_tensor = network.generateRandomTrainingTensor(0.1);
+var step_size = 0.05;
+var training_tensor = network.generateRandomTrainingTensor(step_size);
 var trainingdata = [];
 // for(var i = 0; i < 4000; i++){
 //     var x = Math.random() * 10 - 5;
@@ -26,16 +27,16 @@ for(var i = 0; i < 4000; i++){
     var y = Math.random() * 10 - 5;
 
     trainingdata[i] = {};
-    trainingdata[i].data = [x, y];
+    trainingdata[i].data = [x, y, x*x, y*y, Math.sin(x), Math.sin(y)];
 
-    if( x*x*x*0.5 >= y){
-        trainingdata[i].target = [0];
-    } else {
+    if(x*y*10>=5-y*y){
         trainingdata[i].target = [1];
+    }else {
+        trainingdata[i].target = [0];
     }
 }
 
-setInterval(update, 10);
+setInterval(update, 1);
 var keys = {};
 var error_now = network.getGeneralError(trainingdata);
 var error_before = error_now;
@@ -44,11 +45,18 @@ function update()
 {
     if(keys.KeyR) {
         network.setRandom();
+        iterations = 0;
     }
 
-   advance();
+    if(keys.Space) {
+        network.applyTrainingTensor(network.generateRandomTrainingTensor(1));
+    }
 
-    display();
+    advance();
+
+    if(iterations%10==0){
+        display();
+    }
 }
 
 function advance(){
@@ -61,7 +69,7 @@ function advance(){
 
     } else {
         network.undoTrainingTensor(training_tensor);
-        training_tensor = network.generateRandomTrainingTensor(0.1);
+        training_tensor = network.generateRandomTrainingTensor(step_size);
         error_now = network.getGeneralError(trainingdata);
         error_before = error_now;
     }
@@ -77,24 +85,30 @@ function display() {
         for(var j = -50; j < 50;j++){
 
 
-            ctx.fillStyle = toColour(network.calculate([i/10,j/10]));
-            //ctx.fillStyle="red";
-            ctx.fillRect(i*5+350,-j*5+350,5,5);
+            ctx.fillStyle = toColour(network.calculate([i/10,j/10,(i*i/100),(j*j/100),Math.sin(i/100),Math.sin(j/100)]));
+            ctx.fillRect(i*5+350,-j*5+350,4,4);
         }
     }
 
-    ctx.fillStyle = "rgba(255,0,0.7)";
+    
 
     for(var i = 0; i < trainingdata.length; i++){
         if(trainingdata[i].target == 0){
-            ctx.fillRect(350+trainingdata[i].data[0]*50,350-trainingdata[i].data[1]*50,2,2);
+            ctx.fillStyle = "rgba(255,0,0.7,1)";
+            ctx.fillRect(600+350+trainingdata[i].data[0]*50,350-trainingdata[i].data[1]*50,2,2);
+        }
+        if(trainingdata[i].target == 1){
+            ctx.fillStyle = "rgba(100,100,255,1)";
+            ctx.fillRect(600+350+trainingdata[i].data[0]*50,350-trainingdata[i].data[1]*50,2,2);
         }
     }
 
     ctx.fillStyle = "white";
     ctx.font = "20px Georgia";
-    ctx.fillText("Error: " + error_now, 650,500);
-    ctx.fillText("Iterations: " + iterations, 650,550);
+    ctx.fillText("Error: " + error_now, 100,50);
+    ctx.fillText("Iterations: " + iterations, 700,50);
+    ctx.font = "16px Georgia";
+    ctx.fillText("Space - Jump, R - randomise",100,700);
 }
 
 function toColour(value){
